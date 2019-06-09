2754d2b3a299e99792255a3b11ea35666dc917ba
KaviMD <kaviasher@gmail.com>
2019-06-09 16:28:57 -0700

    Added TCP Testing

--- Testing/UDP Testing/GroundNode.py
+++ Testing/UDP Testing/GroundNode.py
@@ -1,10 +1,10 @@
 import socket
 import time
 
-UDP_IP = "169.254.210.42"
+UDP_IP = "169.254.23.12"#"169.254.210.42"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-	msg, addr = sock.recvfrom(8192)
-	print msg.decode()
+	msg, addr = sock.recvfrom(1024)
+	print(msg.decode())
