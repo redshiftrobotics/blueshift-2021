c5fe31c38fc99689de730314dba9da3fa92dfbbe
Kavi Dey <kaviasher@gmail.com>
2020-01-14 11:46:28 -0800

    increased speed of v4l2 camera interface

--- Testing/v4l2 Testing/test_v4l2_cam.py
+++ Testing/v4l2 Testing/test_v4l2_cam.py
@@ -2,16 +2,16 @@ from v4l2_camera import Camera
 import cv2
 import numpy as np
 import time
-#import io
-#from PIL import Image
 
 c = Camera("/dev/video0",1920,1080,4)
 
+num_images = 100
+
 now = time.time()
-for i in range(10):
+for i in range(num_images):
     img = c.get_frame()
     #print(len(img))
     #print(img)
     #cv2.imwrite("cam-" + str(i) + ".jpg",cv2.imdecode(np.array(list(img)),-1))
 print("took",time.time()-now,"total")
-print(10/(time.time()-now),"fps")
+print(num_images/(time.time()-now),"fps")
